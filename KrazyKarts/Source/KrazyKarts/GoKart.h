bf9e9ab4dc0b0fc517342acc79ffc912dc9d5aa9
geostik <cgiteach@gmail.com>
2017-12-05 13:49:31 +0200

    Steering And Turning Circles

--- KrazyKarts/Source/KrazyKarts/GoKart.h
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -44,9 +44,9 @@ private:
 	UPROPERTY(EditAnywhere)
 		float MaxDrivingForce = 10000;
 
-	// The number of degrees rotated per secondd at full controlThrow (degrees/s)
+	// Minimum radius of the car turning circle at full lock (m)
 	UPROPERTY(EditAnywhere)
-		float MaxDegreesPerSecond = 90;
+		float MinTurningRadius = 10;
 
 	// Highr means more DrAG
 	UPROPERTY(EditAnywhere)
